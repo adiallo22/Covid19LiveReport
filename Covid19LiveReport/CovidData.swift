2f53cfb390fc254189d643a4065695ef4fe73485
abdul diallo <rahimi.diallo96@gmail.com>
2020-03-20 14:25:02 -0400

    covid data struct

---
+++ Covid19LiveReport/CovidData.swift
@@ -0,0 +1,20 @@
+//
+//  covidData.swift
+//  Covid19LiveReport
+//
+//  Created by Abdul Diallo on 3/20/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import Foundation
+
+struct CovidData : Codable {
+    
+    var country : String
+    var last_update : Int
+    var confirmed : Int
+    var deaths : Int
+    var recovered : Int
+    var country_code : String
+    
+}
